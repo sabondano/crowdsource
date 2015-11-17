8aee98c7ab769d1b1f3c8d92afc994c5570cd308
Sebastian Abondano <sabondano1@gmail.com>
2015-11-17 01:32:51 -0700

    Add redis configuration for heroku

--- server.js
+++ server.js
@@ -2,9 +2,17 @@ const express = require('express');
 const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
-const redis = require('redis');
 const path = require('path');
-const client = redis.createClient();
+
+if (process.env.REDISTOGO_URL) {
+  var rtg   = require("url").parse(process.env.REDISTOGO_URL);
+  var client = require("redis").createClient(rtg.port, rtg.hostname);
+
+  client.auth(rtg.auth.split(":")[1]);
+} else {
+  var redis = require('redis');
+  var client = redis.createClient();
+}
 
 const bodyParser = require('body-parser');
 const _ = require('lodash');
